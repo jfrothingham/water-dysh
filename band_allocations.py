60c99d1af2efe74055349eec12ac1ba642c5d0f9
Daniel Bautista <dbautista98@berkeley.edu>
2025-06-03 10:21:21 -0400

    added examle band_allocation.py file

---
+++ band_allocations.py
@@ -0,0 +1,17 @@
+# a dictionary of known band allocations. 
+# this is to be used in the function `plot_band_allocations`
+# and the first level of keys is used to specify which 
+# band will be labeled
+band_allocation_ghz_dict = {"none":{},
+                            'ra-protected-band':{"H": [1.4, 1.427],
+                                                 "OH":[1.6606, 1.6700],
+                                                 "S-band":[2.690,2.700],
+                                                 "C-band":[4.990,5.000],
+                                                 "X-band":[10.68,10.7],
+                                                 "Ku-band":[15.35,15.4],
+                                                 "K-band1":[22.21,22.5],
+                                                 "K-band2":[23.6,24.0],
+                                                 "Ka-band":[31.3,31.8],
+                                                 "Q-band":[42.5,43.5]
+                                                 }
+                            }
