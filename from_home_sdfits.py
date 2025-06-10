b092b956b3b3389bb44eb2c48643329420ef34dc
jfrothin <jafroth@gmail.com>
2025-06-10 14:26:21 -0400

    suppressed excessive info, customized for PF800 RFI scans

--- from_home_sdfits.py
+++ from_home_sdfits.py
@@ -1,59 +1,36 @@
 from GBT_waterfall import * 
 from dysh.fits.gbtfitsload import GBTFITSLoad, GBTOffline
+import warnings
+from astropy.utils.exceptions import AstropyWarning
+
+import logging, sys
+logging.disable(sys.maxsize)
 
 if __name__ == "__main__":
-    # uwbr
-    #ryan_uwbr_tests = ["TGBT25A_607_01", "TGBT25A_607_02", "TGBT25A_607_03", "TGBT25A_607_04",]
-    #uwbr_rfi_scans = ["TRFI_050825_251", "TRFI_051524_251", "TRFI_111424_251"]
-    # ods_tests = [#"TGBT25A_607_04", # didn't plot for some reason
-    #              "TGBT25A_607_05", # didn't plot for some reason
-    #              "TGBT25A_607_06", 
-    #              "TGBT25A_607_07", 
-    #              "TGBT25A_607_08",
-    #              "TGBT25A_607_09",
-    #              "TGBT25A_607_10",
-    #              "TGBT25A_607_11",
-    #             #  "TGBT25A_607_12", # ran in vegas pulsar mode
-    #             #  "TGBT25A_606_08", # cyclic spectroscopy (pulsar mode data)
-    #             #  "TGBT25A_606_09", # cyclic spectroscopy (pulsar mode data)
-    #             #  "TGBT25A_606_10", # cyclic spectroscopy (pulsar mode data)
-    #             #  "TGBT25A_606_11", # cyclic spectroscopy (pulsar mode data)
-    #             #  "TGBT25A_606_12", # cyclic spectroscopy (pulsar mode data)
-    #              ]
-
-    # x-band
-    remijan_rfi = ["AGBT25A_218_23"]
-    nrdz_spaceX_tests = ["TGBT24B_609_01","TGBT24B_609_02","TGBT24B_609_03","TGBT24B_609_05","TGBT24B_609_05"]
-    # xband_rfi_scans = ["TRFI_020325_X1", # completed -- do not need to run again
-    #         #"TRFI_033025_X1", # this one broke -- look into why
-    #         # "TRFI_040625_X1", # this one broke -- look into why
-    #         "TRFI_041125_X1",
-    #         "TRFI_050424_X1",
-    #         "TRFI_050524_X1",
-    #         "TRFI_050624_X1",
-    #         "TRFI_050924_X1",
-    #         "TRFI_051124_X1",
-    #         "TRFI_051224_X1",
-    #         "TRFI_051824_X1",
-    #         "TRFI_060124_X1",
-    #         "TRFI_060224_X1",
-    #         "TRFI_060224_X2",
-    #         "TRFI_060324_X1",
-    #         "TRFI_101924_X1",
-    #         "TRFI_112524_X1",
-    #         "TRFI_120624_X1",
-    #         "TRFI_122824_X1",
-    #         # "TRFI_122924_X1", # this one broke -- look into why
-    #         "TRFI_123124_X1"
-    #         ]
-    older_xband = ["TRFI_123124_X1"]
-    test_list = ["AGBT25A_155_01"]
-
-    # ku-band 
-    ku_band_post_spaceX_sessions = ["TRFI_123124_U1", "TRFI_122424_U1", "TRFI_122224_U1","TRFI_122024_U1"]
     
-    #from_archive = True
-    outdir = "/home/scratch/jfrothin/RFI/plots/"
+
+    PF800_RFI = ["TRFI_010125_81",
+                        "TRFI_011625_81",
+                        "TRFI_021425_81",
+                        "TRFI_022125_81",
+                        "TRFI_050424_81",
+                        "TRFI_050524_81",
+                        "TRFI_050624_81",
+                        "TRFI_050924_81",
+                        "TRFI_051124_81",
+                        "TRFI_051224_81",
+                        "TRFI_052724_81",
+                        "TRFI_053124_81",
+                        "TRFI_101924_81",
+                        "TRFI_112924_81",
+                        "TRFI_121324_81",
+                        "TRFI_122124_81",
+                        "TRFI_122624_81",
+                        "TRFI_122824_81",
+                        "TRFI_123124_81"]
+
+    outdir_raw = "/users/jfrothin/RFI/plots/raw/"
+    outdir_med = "/users/jfrothin/RFI/plots/median/"
 
     # for session_ID in test_list:
     #     sdf = GBTOffline(session_ID) # for data in /home/sdfits
@@ -72,13 +49,17 @@ if __name__ == "__main__":
     #                   outdir=outdir, 
     #                   cal_type="median_subtract")
     
-    session_ID = "TRFI_123124_U1"
+    session_ID = PF800_RFI[0]
     sdf = GBTOffline(session_ID) # for data in /home/sdfits
     
     GBT_waterfall(sdf, session_ID,  
-                  outdir=outdir, 
+                  fmin_GHz=0.75,
+                  outdir=outdir_raw, 
+                  band_allocation="25B-184",
                   cal_type="raw_data")
     
-    GBT_waterfall(sdf, session_ID, 
-                  outdir=outdir, 
+    GBT_waterfall(sdf, session_ID,
+                  fmin_GHz=0.75,
+                  outdir=outdir_med, 
+                  band_allocation="25B-184",
                   cal_type="median_subtract")
